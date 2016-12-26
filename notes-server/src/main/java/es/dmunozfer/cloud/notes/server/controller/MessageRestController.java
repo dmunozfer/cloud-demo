bb10c8164fda81569910e3251499f6b24fa0036d
SERBATIC-03474\David Muñoz <damufe@gmail.com>
2016-12-26 23:28:13 +0100

    añadido controller hello

---
+++ notes-server/src/main/java/es/dmunozfer/cloud/notes/server/controller/MessageRestController.java
@@ -0,0 +1,26 @@
+package es.dmunozfer.cloud.notes.server.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by David Muñoz on 26/12/2016.
+ */
+@RestController
+@RefreshScope
+public class MessageRestController {
+    private final String message;
+
+    @Autowired
+    public MessageRestController(@Value("${message}") String message) {
+        this.message = message;
+    }
+
+    @GetMapping(value = "/message")
+    public String read() {
+        return message;
+    }
+}
