5bcf9cabd07dfb4816fb42f41bb62c1fe4147074
SERBATIC-03474\David Muñoz <damufe@gmail.com>
2016-12-27 15:04:44 +0100

    añadido script build & run docker images/containers

---
+++ build-run-docker.ps1
@@ -0,0 +1,27 @@
+# Elimina todos los contenedores de la aplicación
+docker rm -f scd-notes-server
+docker rm -f scd-eureka-server
+docker rm -f scd-config-server
+
+# Elimina todas las imágenes de la aplicación
+docker rmi spring_cloud_demo/notes-server
+docker rmi spring_cloud_demo/eureka-server
+docker rmi spring_cloud_demo/config-server
+
+# Regenera las imágenes de la aplicación
+cd .\config-server\
+mvn package docker:build -DskipTests
+cd ..
+
+cd .\eureka-server\
+mvn package docker:build -DskipTests
+cd ..
+
+cd .\notes-server\
+mvn package docker:build -DskipTests
+cd ..
+
+# Crea y ejecuta los contenedores de la aplicación
+docker run --network=scd_network --name scd-config-server -h scd-config-server -p 8888:8888 -d spring_cloud_demo/config-server
+docker run --network=scd_network --name scd-eureka-server -h scd-eureka-server -p 8761:8761 -d spring_cloud_demo/eureka-server
+docker run --network=scd_network --name scd-notes-server -h scd-notes-server -p 8000:8000 -d spring_cloud_demo/notes-server
