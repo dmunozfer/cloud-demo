0809c27b6d022dcb2b0f2615b00c8f258e19cfa9
SERBATIC-03474\David Muñoz <damufe@gmail.com>
2016-12-28 13:01:59 +0100

    añadido lectura de mensaje colas en notes-server

--- notes-server/src/main/java/es/dmunozfer/cloud/notes/server/NotesServerApplication.java
+++ notes-server/src/main/java/es/dmunozfer/cloud/notes/server/NotesServerApplication.java
@@ -1,10 +1,18 @@
 package es.dmunozfer.cloud.notes.server;
 
+import es.dmunozfer.cloud.notes.server.model.Note;
+import es.dmunozfer.cloud.notes.server.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.SubscribableChannel;
 
+@EnableBinding(NotesChannel.class)
 @EnableDiscoveryClient
 @SpringBootApplication
 public class NotesServerApplication {
@@ -13,4 +21,25 @@ public class NotesServerApplication {
         SpringApplication.run(NotesServerApplication.class, args);
     }
 
+}
+
+interface NotesChannel {
+    @Input
+    SubscribableChannel input();
+}
+
+@MessageEndpoint
+class NoteProcessor {
+
+    private final NoteRepository noteRepository;
+
+    @Autowired
+    NoteProcessor(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    @ServiceActivator(inputChannel = "input")
+    public void onNewNote(String text) {
+        this.noteRepository.save(new Note(text));
+    }
 }
