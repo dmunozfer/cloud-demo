1526a76d6db7f2bdfae569252846919b4bb19f0c
SERBATIC-03474\David Muñoz <damufe@gmail.com>
2016-12-29 21:06:06 +0100

    cambiado zipkin rest por stream con rabbitmq

--- zipkin-server/src/main/java/es/dmunozfer/cloud/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/es/dmunozfer/cloud/ZipkinServerApplication.java
@@ -3,9 +3,9 @@ package es.dmunozfer.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import zipkin.server.EnableZipkinServer;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
-@EnableZipkinServer
+@EnableZipkinStreamServer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ZipkinServerApplication {
