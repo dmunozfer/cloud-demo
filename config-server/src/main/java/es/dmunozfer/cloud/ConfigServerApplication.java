4a1669e49b9538717e61641cd80eb257d34dbd0e
SERBATIC-03474\David Muñoz <damufe@gmail.com>
2016-12-29 23:42:53 +0100

    Config server ahora se registra en eureka y usa cloud stream bus para propagar los cambios de configuración

--- config-server/src/main/java/es/dmunozfer/cloud/ConfigServerApplication.java
+++ config-server/src/main/java/es/dmunozfer/cloud/ConfigServerApplication.java
@@ -2,10 +2,11 @@ package es.dmunozfer.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
